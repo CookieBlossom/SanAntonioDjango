c1c52e426e00b6eb19abee1ba991c3f19a4c4d78
CookieBlossom <mariajesuss2424@gmail.com>
2024-07-13 00:55:17 -0400

    settings 11

--- sanantonio/settings.py
+++ sanantonio/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviroment.get('SECRET KEY', default = 'whatts1334')
+SECRET_KEY = os.environ.get('SECRET KEY', default = 'whatts1334')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
