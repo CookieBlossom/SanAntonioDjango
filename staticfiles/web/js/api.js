4bb8a6469b2241c5b31b87cd8174caf0d5ac9753
CookieBlossom <mariajesuss2424@gmail.com>
2024-07-12 23:08:09 -0400

    api.js

--- staticfiles/web/js/api.js
+++ staticfiles/web/js/api.js
@@ -1,4 +1,10 @@
-let API_BASE_URL = "http://127.0.0.1:8000/"
+let API_BASE_URL = "http://127.0.0.1:8000/" // URL de la API para entorno de desarrollo local
+
+if (window.location.hostname === "sanantoniodjango.onrender.com") {
+    // URL de la API para entorno de producción (Render)
+    API_BASE_URL = "https://sanantoniodjango.onrender.com";
+}
+
 
 export const getProducts = async() => {
     try{
