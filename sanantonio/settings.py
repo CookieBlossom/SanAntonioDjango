e12698a547437f997e11de44efec899a9a6bc7bc
CookieBlossom <mariajesuss2424@gmail.com>
2024-07-12 23:44:40 -0400

    puerto

--- sanantonio/settings.py
+++ sanantonio/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+PORT = os.environ.get('PORT', 8000)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- sanantonio/wsgi.py
+++ sanantonio/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from django.core.management import execute_from_command_line
+from .settings import PORT
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanantonio.settings')
 
 application = get_wsgi_application()
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tu_proyecto.settings')
+execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{PORT}'])
